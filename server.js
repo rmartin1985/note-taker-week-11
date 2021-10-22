f7a9c933e44b065742a6fb218a632a3f073081f5
Rick Martin <rmartin1985@att.net>
2021-10-22 17:11:43 -0500

    added route for the javascript and css to load

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const { db } = require('./db/db');
 // Middleware functions
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
     res.json(db);
